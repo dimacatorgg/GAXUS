2082448dcb821e867a6fd746c3fa329973c060b0
dimacatorgg <radocaj.dimitrije@jjzmaj.edu.rs>
2025-05-10 12:48:17 +0200

    sdasd

--- src/gmail.vue
+++ src/gmail.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref,computed,watch} from "vue"
+import {ref,computed,watch,watchEffect} from "vue"
     const vrednsoti = ref(["","","","","",""]);
     const active = ref([0])
   
@@ -16,17 +16,36 @@ for(var i = 0;i<vrednsoti.value.length;i++){
         active.value[0] = i;
     })
 }*/
+const inputs = ref(null)
+const vlls = ref(document.querySelectorAll("input"));
 const some = ref(-1);
 setInterval(function(){
     for(var i=0;i<document.querySelectorAll("input").length;i++){
     if(document.querySelectorAll("input")[i] === document.activeElement){
         some.value = i;
     }
+
 }
 },100)
-
-   window.onkeyup = function(){
-        console.log(some.value)
+watchEffect(() => {
+    if( inputs.value!=null){
+    console.log(inputs.value[0].value)
+    }
+})
+   window.onkeyup = function(e){
+    
+        if(some.value>=0 && e.key!="Backspace" && some.value!=vrednsoti.value.length-1){
+            if(document.querySelectorAll("input")[some.value].value != ""){
+                document.querySelectorAll("input")[some.value +1].focus();
+                
+             //   document.querySelectorAll("input")[some.value +1].innerHTML = e.key;
+            }
+        }else if(some.value!=0 && e.key=="Backspace"){
+            if(document.querySelectorAll("input")[some.value].value == ""){
+                document.querySelectorAll("input")[some.value-1].focus();
+               
+            }
+        }
    }
 </script>
 
@@ -64,7 +83,7 @@ setInterval(function(){
                 <div class="code">
                  
                     <div class="codes" v-for="(code,index) in vrednsoti">
-                        <input v-model="vrednsoti[index]" maxlength="1"/>
+                        <input v-model="vrednsoti[index]" maxlength="1"  ref="inputs" />
                        
                     </div>
                 </div>
