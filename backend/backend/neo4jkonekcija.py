ba28e4448217d39a2d6dbe7af4f3c5894c7de4d2
dimacatorgg <radocaj.dimitrije@jjzmaj.edu.rs>
2025-06-09 17:49:55 +0200

    Nesto novo

--- backend/backend/neo4jkonekcija.py
+++ backend/backend/neo4jkonekcija.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://localhost:7687",auth=("neo4j","12345678"))
+driver = GraphDatabase.driver("neo4j+s://3691edef.databases.neo4j.io",auth=("neo4j","Vx4MYPlC0a-0-IcvsvwcDPeQ5m0sr3LxWraYjXhufiw"))
 def ikok(ok,password,gmail,id):
   
     with driver.session() as session:
--- backend/registracija/models.py
+++ backend/registracija/models.py
@@ -2,3 +2,4 @@ from django.db import models
 class Post(models.Model):
     name = models.CharField(max_length=100)
     idx = models.CharField(max_length=1000)
+#Hej ovde ima nesto novo
